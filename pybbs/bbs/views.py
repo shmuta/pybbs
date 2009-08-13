40a28afe4d0f6530b02bfa538378590d105b9f87
ekondrashev <ekondrashev@ekondrashev.(none)>
2009-08-13 19:42:32 +0300

    Added emutavchi superuser

--- pybbs/bbs/views.py
+++ pybbs/bbs/views.py
@@ -1,4 +1,7 @@
+from pybbs.bbs.models import Message
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the bbs index.")
+    latest_message_list = Message.objects.all().order_by('-post_date')[:5]
+    output = ', '.join([message.name for message in latest_message_list])
+    return HttpResponse(output)
