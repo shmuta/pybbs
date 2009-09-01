8fcc464130058db241aa163cadb8df79e202c309
ekondrashev <ekondrashev@ekondrashev.(none)>
2009-09-01 16:20:19 +0300

    Removed choices data from model

--- pybbs/bbs/models.py
+++ pybbs/bbs/models.py
@@ -13,45 +13,30 @@ class Message(models.Model):
         return self.title
 
 class Label(models.Model):
-    LABEL_TYPES = (
-        (u'c1', u'label1'),
-        (u'c2', u'label2'),
-    )
-    name = models.CharField(max_length=50, choices=LABEL_TYPES)
+    name      = models.CharField(max_length=50)
 
     def __unicode__(self):
         return self.name
 
 class LabelList(models.Model):
-    message = models.ForeignKey(Message)
-    label = models.ForeignKey(Label)
-    user = models.ForeignKey(User)
+    message   = models.ForeignKey(Message)
+    label     = models.ForeignKey(Label)
+    user      = models.ForeignKey(User)
 
     def __unicode__(self):
         return self.user + ' ' + self.label
 
 class RatingList(models.Model):
-    message = models.ForeignKey(Message)
-    RATINGS = (
-        (u'1', u'r1'),
-        (u'2', u'r2'),
-    )
-    rating = models.CharField(max_length=10, choices=RATINGS)
-    user = models.ForeignKey(User)
+    message   = models.ForeignKey(Message)
+    rating    = models.CharField(max_length=10)
+    user      = models.ForeignKey(User)
 
     def __unicode__(self):
         return self.user.username + ' ' + self.rating
 
 class CategoryList(models.Model):
-    CATEGORIES = (
-        (u'root', u'Root category'),
-        (u'c1', u'test category1'),
-        (u'c2', u'test category2'),
-        (u'c3', u'test category3'),
-        (u'c4', u'test category4'),
-    )
-    category = models.CharField(max_length=50, choices=CATEGORIES)
-    message = models.ForeignKey(Message)
+    category  = models.CharField(max_length=50)
+    message   = models.ForeignKey(Message)
 
     def __unicode__(self):
         return self.message + ' ' + self.category
